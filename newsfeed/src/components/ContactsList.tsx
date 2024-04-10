02f198993be660db21d2496d7b46f7689643ae4c
Eugene Kong <eugkong@gmail.com>
2024-04-11 00:00:17 +0800

    Refetchable fragments

--- newsfeed/src/components/ContactsList.tsx
+++ newsfeed/src/components/ContactsList.tsx
@@ -1,8 +1,11 @@
+import { debounce } from "lodash";
 import * as React from "react";
-import { useFragment } from "react-relay";
-import { graphql } from "relay-runtime";
+import { useRefetchableFragment } from "react-relay";
+import { Options } from "react-relay/relay-hooks/useRefetchableFragmentNode";
+import { Variables, graphql } from "relay-runtime";
 import Card from "./Card";
 import ContactRow from "./ContactRow";
+import SearchInput from "./SearchInput";
 import type { ContactsListFragment$key } from "./__generated__/ContactsListFragment.graphql";
 
 export type Props = {
@@ -10,8 +13,10 @@ export type Props = {
 };
 
 const ContactsListFragment = graphql`
-  fragment ContactsListFragment on Viewer {
-    contacts {
+  fragment ContactsListFragment on Viewer
+  @refetchable(queryName: "ContactsListRefetchQuery")
+  @argumentDefinitions(search: { type: "String", defaultValue: null }) {
+    contacts(search: $search) {
       id
       ...ContactRowFragment
     }
@@ -19,10 +24,40 @@ const ContactsListFragment = graphql`
 `;
 
 export default function ContactsList({ viewer }: Props): React.ReactElement {
-  const data = useFragment(ContactsListFragment, viewer);
+  const [data, refetch] = useRefetchableFragment(ContactsListFragment, viewer);
+  const [searchString, setSearchString] = React.useState("");
+  const [isPending, startTransition] = React.useTransition();
+
+  const refetchDebounced = React.useMemo(
+    () =>
+      debounce(
+        (vars: Partial<Variables>, options?: Options) =>
+          /**
+           * Transitions are React state updates that do not need to be immediately
+           * responded to — React can wait until the data is available.
+           */
+          startTransition(() => {
+            refetch(vars, options);
+          }),
+        200
+      ),
+    []
+  );
+
+  const onSearchStringChanged = (value: string) => {
+    setSearchString(value);
+    refetchDebounced({ search: value });
+  };
+
   return (
     <Card dim={true}>
       <h3>Contacts</h3>
+      <SearchInput
+        value={searchString}
+        onChange={onSearchStringChanged}
+        isPending={isPending}
+      />
+
       {data.contacts.map((contact) => (
         <ContactRow key={contact.id} contact={contact} />
       ))}
